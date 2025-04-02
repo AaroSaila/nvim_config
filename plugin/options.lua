1c4c36cc384ad3e4a5af2ed375308b7de6ebc068
Aaro Saila <aaros.saila@gmail.com>
2025-04-02 21:14:18 +0300

    informative comments

--- plugin/options.lua
+++ plugin/options.lua
@@ -2,8 +2,8 @@ vim.cmd("colorscheme moonfly")
 vim.cmd("set nu")
 vim.cmd("set rnu")
 vim.cmd("set expandtab")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")  -- Sets shift width
+vim.cmd("set softtabstop=2") -- Do not change!
 vim.cmd("set nowrap")
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 vim.opt.scrolloff = 8
